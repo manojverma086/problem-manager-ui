5017d1ccd716daa46fe6b80f2f61a22e71133ed0
manoj <verma.manoj241@gmail.com>
2019-08-05 23:22:29 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:3000/problems/';
+export const API_URL = 'https://pmanager-api.herokuapp.com/problems/';
 export const QUESTION_SAVE = 'internal/question/save';
 export const QUESTION_UPDATE = 'internal/question/update';
 export const QUESTION_ADD = 'internal/question/add';
